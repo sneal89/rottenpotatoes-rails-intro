81cd807f939ba8aa82f7b68b835dd108d792c316
EC2 Default User <ec2-user@ip-172-31-22-203.us-east-2.compute.internal>
2020-02-07 23:02:25 +0000

    testing19

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,15 +12,20 @@ class MoviesController < ApplicationController
 
   def index
     
-    if params[:sort].nil? && params[:ratings].nil? &&
-        (!session[:sort].nil? || !session[:ratings].nil?)
+    if params[:sort].nil? && params[:ratings].nil? && (!session[:sort].nil? || !session[:ratings].nil?)
       redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
     end
-    
+    #@ratings = params[:ratings]
+    #if @ratings.nil?
+    #  ratings = params[:sort]
+    #else
+    #  ratings = @ratings.keys
+    #end
     @sort = params[:sort]
     @movies = Movie.all.order(@sort)
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
     @checked_ratings = check_ratings
+    
     @checked_ratings.each do |rating|
       params[rating] = true
     end
