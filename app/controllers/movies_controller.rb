15853b2ae8e2b17af31dcf536f280270beaa86bc
EC2 Default User <ec2-user@ip-172-31-22-203.us-east-2.compute.internal>
2020-02-08 02:41:22 +0000

    testingback6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
     @t_param = params[:ratings] || session[:ratings]
     session[:sort] = @sort
     session[:ratings] = @t_param
-    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    @movies = Movie.where(rating: session[:ratings]).order(session[:sort])
     if(params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
       flash.keep
       redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
