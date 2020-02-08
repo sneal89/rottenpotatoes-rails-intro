59642791234c7412f3d907e4dab909e66d2d0ee4
EC2 Default User <ec2-user@ip-172-31-22-203.us-east-2.compute.internal>
2020-02-08 01:16:09 +0000

    testingback1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,8 @@ class Movie < ActiveRecord::Base
 
     #attr_accessor :title, :rating, :description, :release_date
 
-def self.ratings
-    ['G', 'PG', 'PG-13', 'R']
-end
+#def self.ratings
+#    ['G', 'PG', 'PG-13', 'R']
+#end
 
 end
