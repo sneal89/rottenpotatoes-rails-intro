25bb3bee9de91a3071a87a1e4dcf30dd2328783c
EC2 Default User <ec2-user@ip-172-31-22-203.us-east-2.compute.internal>
2020-02-08 02:23:48 +0000

    testingback5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,9 +12,19 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.ratings
-    @sort = params[:sort]
-    params[:ratings].nil? ? t_param = @all_ratings : t_param = params[:ratings].keys
-    @movies = Movie.where(rating: t_param).order(@sort)
+    @sort = params[:sort] || session[:sort]
+    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
+    @t_param = params[:ratings] || session[:ratings]
+    session[:sort] = @sort
+    session[:ratings] = @t_param
+    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    if(params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
+      flash.keep
+      redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
+    end
+    
+    #params[:ratings].nil? ? t_param = @all_ratings : t_param = params[:ratings].keys
+    #@movies = Movie.where(rating: t_param).order(@sort)
     
   end
 
