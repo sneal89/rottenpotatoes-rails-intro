240f3877053c17d66115f68a8bc93ccb1e38c4bc
EC2 Default User <ec2-user@ip-172-31-22-203.us-east-2.compute.internal>
2020-02-08 02:00:10 +0000

    testingback3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,17 @@
 class Movie < ActiveRecord::Base
 
+    def self.ratings
+        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating}
+    end
+
     #attr_accessor :title, :rating, :description, :release_date
 
 #def self.ratings
 #    ['G', 'PG', 'PG-13', 'R']
 #end
 
-  def self.ratings
-    return ['G','PG','PG-13','R']
-  end
+  #def self.ratings
+  ##  return ['G','PG','PG-13','R']
+  #end
 
 end
